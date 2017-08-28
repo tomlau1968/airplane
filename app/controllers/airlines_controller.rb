bc03159fa703f9155363d159549fc160b094e3e3
tomtom <info@tomlau.com.au>
2017-08-28 14:23:40 +1000

    first commit

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -8,6 +8,39 @@ end
 
 def new
   @airline = Airline.new
-
   end
+
+def create
+airline = Airline.create airline_params
+redirect_to airline
+end
+
+def show
+@airline = Airline.find params[:id]
+end
+
+def edit
+  @airline = Airline.find params[:id]
+end
+
+def show
+  @airline = Airline.find params[:id]
+end
+
+def edit
+@airline = Airline.find params[:id]
+end
+
+def update
+  airline =Airline.find params[:id]
+  airline.update airline_params
+  redirect_to airline
+end
+
+def destory
+  airline =Airline find params [:id]
+  airline.destory
+  redirect_to airlines_path
+end
+
 end
--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -8,14 +8,34 @@ def new
   @airplane = Airplane.new
   end
 
-def create
-  airplane = Airplane.create :name => params[:name], :image => params[:image], :engine => params[:engine], :speed => params[:speed], :passenger => params[:passenger], :weight => params[:weight]
-  redirect_to airplane_path(airplane.id)
-end
+  def create
+      airplane = Airplane.create airplane_params
+      redirect_to airplane
+    end
 
 def show
 @airplane = Airplane.find params[:id]
-
 end
+
+def edit
+  @airplane = Airplane.find params[:id]
+end
+
+def update
+    airplane = Airplane.find params[:id]
+    airplane.update airplane_params
+    redirect_to airplane
+  end
+
+def destroy
+  airplane = Airplane.find params[:id]
+   airplane.destroy
+   redirect_to airplanes_path
+end
+
+private
+  def airplane_params
+    params.require(:airplane).permit(:name, :engine, :speed, :passenger, :weight, :image) #
+  end
 
 end
