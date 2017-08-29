c1a862a6a74fc6fa53cd5633f4ef59885171549e
tomtom <info@tomlau.com.au>
2017-08-30 09:36:51 +1000

    still sex it up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Airplane.destroy_all
-ap1 = Airplane.create :name => 'QF123', :engine => 'supergood 2.7', :speed => '900mph', :passenger => 235, :weight => 20000
+ap1 = Airplane.create :name => 'Boeing 787 Dreamliner', :engine => 'Trent 1000 TEN', :speed => '945 km/h', :passenger => 420, :weight => 128850, :image => "http://www.boeing.com/resources/boeingdotcom/commercial/787/assets/images/787-10_marquee.jpg"
 
 
 Airline.destroy_all
-al1 = Airline.create :name => 'Syney International Airport', :location => 'Sydney', :num_plane => 500, :fuel =>20000, :type_plane => 'QF123'
+al1 = Airline.create :name => 'Qantas', :location => 'Australia', :num_plane => 122, :fuel => 7700000, :type_plane => 'Airbus A330-200, Airbus A380-800, Boeing 737–800, Boeing 747-400, Boeing 787–9', :image => "https://blog.aragroup.com.au/wp-content/uploads/sites/19/2014/10/Qantas_140129_4851.jpg"
